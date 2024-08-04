fc49493d1f2eceeef8d8bce54c9be399815f0773
Tristan-Phillips <81848746+Tristan-Phillips@users.noreply.github.com>
2024-08-04 11:25:19 +0200

    remove non registerable services

--- public/script/config/home.js
+++ public/script/config/home.js
@@ -9,13 +9,9 @@ const CONFIG = {
   
     const COMMANDS = new Map([
       ['c', { name: 'Cyberchef', url: 'https://url.trap.lol/cyberchef' }],
-      ['d', { name: 'Database', url: 'https://url.trap.lol/db' }],
       ['e', { name: 'ScreenShare', url: 'https://url.trap.lol/screenshare' }],
-      ['f', { name: 'RSS Feed', url: 'https://url.trap.lol/rss' }],
       ['i', { name: 'IT-Tools', url: 'https://url.trap.lol/it-tools' }],
-      ['m', { name: 'Memos', url: 'https://url.trap.lol/memo' }],
       ['p', { name: 'PDF Tools', url: 'https://url.trap.lol/pdf' }],
-      ['r', { name: 'Recipes', url: 'https://url.trap.lol/recipe' }],
       ['s', { name: 'PW Share', url: 'https://url.trap.lol/pwpush' }],
       ['t', { name: 'SpeedT', url: 'https://url.trap.lol/speedtest' }],
       ['w', { name: 'Web Check', url: 'https://url.trap.lol/webcheck' }],
