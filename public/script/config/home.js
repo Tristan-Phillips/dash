9c1ffab42f1dcbd09f6b90426882db1164443c14
Tristan-Phillips <81848746+Tristan-Phillips@users.noreply.github.com>
2024-08-10 17:57:56 +0200

    VPS cancel

--- public/script/config/home.js
+++ public/script/config/home.js
@@ -8,15 +8,6 @@ const CONFIG = {
     };
   
     const COMMANDS = new Map([
-      ['c', { name: 'Cyberchef', url: 'https://url.trap.lol/cyberchef' }],
-      ['e', { name: 'ScreenShare', url: 'https://url.trap.lol/screenshare' }],
-      ['i', { name: 'IT-Tools', url: 'https://url.trap.lol/it-tools' }],
-      ['p', { name: 'PDF Tools', url: 'https://url.trap.lol/pdf' }],
-      ['s', { name: 'PW Share', url: 'https://url.trap.lol/pwpush' }],
-      ['t', { name: 'SpeedT', url: 'https://url.trap.lol/speedtest' }],
-      ['w', { name: 'Web Check', url: 'https://url.trap.lol/webcheck' }],
-      ['y', { name: 'YT Download', url: 'https://url.trap.lol/ytdlp' }],
-      ['z', { name: 'WhiteBoard', url: 'https://url.trap.lol/wbo' }],
       [
         '0',
         {
