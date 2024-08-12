5801a62237b014888d463c76d278d171badd47a1
Tristan-Phillips <81848746+Tristan-Phillips@users.noreply.github.com>
2024-08-12 07:19:23 +0200

    livetv removed

--- public/script/config/tools.js
+++ public/script/config/tools.js
@@ -11,7 +11,6 @@ const CONFIG = {
     ['c', { name: 'Cyberchef', url: 'https://url.trap.lol/cyberchef' }],
     ['i', { name: 'It-Tools', url: 'https://url.trap.lol/it-tools' }],
     ['p', { name: 'PDF', url: 'https://url.trap.lol/pdf' }],
-    ['d', { name: 'LiveTV', url: 'https://url.trap.lol/thetvapp' }],
     [
       '0',
       {
