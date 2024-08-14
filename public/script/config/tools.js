4f6aabe15af887fb6b20fd166e5bfb9ec9fa1706
Tristan-Phillips <81848746+Tristan-Phillips@users.noreply.github.com>
2024-08-14 08:23:29 +0200

    tools added

--- public/script/config/tools.js
+++ public/script/config/tools.js
@@ -9,8 +9,10 @@ const CONFIG = {
 
   const COMMANDS = new Map([
     ['c', { name: 'Cyberchef', url: 'https://url.trap.lol/cyberchef' }],
+    ['e', { name: 'Exdraw', url: 'https://url.trap.lol/exdraw' }],
     ['i', { name: 'It-Tools', url: 'https://url.trap.lol/it-tools' }],
     ['p', { name: 'PDF', url: 'https://url.trap.lol/pdf' }],
+    ['r', { name: 'Readme', url: 'https://url.trap.lol/readme' }],
     [
       '0',
       {
